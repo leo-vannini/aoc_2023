fc50617a671026a7a852e4c028eff7a338a663cb
Leo Vannini <leovannini21@gmail.com>
2023-12-02 19:45:13 -0800

    day 2 part 2 solution, refactor part 1

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,3 @@
-use std::collections::HashMap;
-
-const DEBUG: bool = false;
-
 pub fn part1(input: &str) -> i32 {
     let mut accumulator: i32 = 0;
     for line in input.lines() {
@@ -10,41 +6,35 @@ pub fn part1(input: &str) -> i32 {
         let full: Vec<&str> = line.split(":").collect();
         let begining: Vec<&str> = full[0].split(" ").collect();
         let game_id: i32 = begining[1].parse::<i32>().unwrap();
-
-        // parse until ; OR newline
-        if DEBUG {
-            println!("{game_id}");
-        }
-
         let mut valid: bool = true;
 
         'parse_game: for game in full[1].split(";") {
-            if DEBUG {
-                println!("Game string --> {game}");
-            }
+            let mut cube = Cube {
+                red: 12,
+                green: 13,
+                blue: 14,
+            };
 
             for play in game.split(",") {
-                let mut cfg: HashMap<&str, i32> =
-                    HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
-
-                if DEBUG {
-                    println!("Play --> {play}");
-                }
-
                 // TODO --> can you destructure cleaner?
                 let op: Vec<&str> = play.trim().split(" ").collect();
                 let num: i32 = op[0].parse::<i32>().unwrap();
                 let color: &str = op[1];
-                if DEBUG {
-                    println!("Num --> {}", num);
-                    println!("Color --> {}", color);
+
+                match color {
+                    "green" => {
+                        cube.green -= num;
+                    }
+                    "red" => {
+                        cube.red -= num;
+                    }
+                    "blue" => {
+                        cube.blue -= num;
+                    }
+                    _ => println!("BAD COLOR"),
                 }
 
-                let current: i32 = cfg.get(color).copied().unwrap();
-                let possible: i32 = current - num;
-                if possible >= 0 {
-                    cfg.insert(color, current - num);
-                } else {
+                if cube.green < 0 || cube.red < 0 || cube.blue < 0 {
                     valid = false;
                     break 'parse_game;
                 }
@@ -53,9 +43,58 @@ pub fn part1(input: &str) -> i32 {
 
         if valid {
             accumulator += game_id
-        } else {
-            println!("{game_id} --> IMPOSSIBLE!")
         }
+    }
+
+    return accumulator;
+}
+
+struct Cube {
+    red: i32,
+    blue: i32,
+    green: i32,
+}
+
+pub fn part2(input: &str) -> i32 {
+    let mut accumulator: i32 = 0;
+
+    for line in input.lines() {
+        let mut cube = Cube {
+            red: 0,
+            blue: 0,
+            green: 0,
+        };
+
+        let v: Vec<&str> = line.split(":").collect();
+        for game in v[1].split(";") {
+            for play in game.split(",") {
+                // TODO --> can you destructure cleaner?
+                let op: Vec<&str> = play.trim().split(" ").collect();
+                let num: i32 = op[0].parse::<i32>().unwrap();
+                let color: &str = op[1];
+
+                match color {
+                    "green" => {
+                        if num > cube.green {
+                            cube.green = num
+                        }
+                    }
+                    "red" => {
+                        if num > cube.red {
+                            cube.red = num
+                        }
+                    }
+                    "blue" => {
+                        if num > cube.blue {
+                            cube.blue = num
+                        }
+                    }
+                    _ => println!("BAD COLOR"),
+                }
+            }
+        }
+
+        accumulator += cube.green * cube.red * cube.blue;
     }
 
     return accumulator;
@@ -64,16 +103,26 @@ pub fn part1(input: &str) -> i32 {
 mod tests {
     use super::*;
 
-    const EXAMPLE_1: &str = include_str!("../days/2/example_1");
+    const EXAMPLE: &str = include_str!("../days/2/example");
     const INPUT: &str = include_str!("../days/2/input");
 
     #[test]
     pub fn part1_example() {
-        assert_eq!(part1(EXAMPLE_1), 8);
+        assert_eq!(part1(EXAMPLE), 8);
     }
 
     #[test]
     pub fn part1_input() {
         assert_eq!(part1(INPUT), 2449)
+    }
+
+    #[test]
+    pub fn part2_example() {
+        assert_eq!(part2(EXAMPLE), 2286)
+    }
+
+    #[test]
+    pub fn part2_input() {
+        assert_eq!(part2(INPUT), 63981)
     }
 }
